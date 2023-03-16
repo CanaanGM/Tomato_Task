3516d42d4fa5aac080a15e917135e622fecebbd2
CanaanGM <reakak1@gmail.com>
2023-03-16 23:38:58 +0300

    formatting via black

--- task/organize_language_files.py
+++ task/organize_language_files.py
@@ -1,6 +1,6 @@
-
-import  os, shutil, argparse
+import os, shutil, argparse
 from typing import Tuple
+
 
 def create_dir_if_doesnt_exist(dir_path: str) -> None:
     """checks if the dir is there, and creates it if not
@@ -12,7 +12,7 @@ def create_dir_if_doesnt_exist(dir_path: str) -> None:
         os.makedirs(dir_path)
 
 
-def move_file(file_path: str,dir_path:str ) -> None:
+def move_file(file_path: str, dir_path: str) -> None:
     """copies the files from one dir to another
 
     Args:
@@ -21,31 +21,44 @@ def move_file(file_path: str,dir_path:str ) -> None:
     """
     shutil.move(file_path, dir_path)
 
+
 def parse_args() -> Tuple[str, str]:
     """parses the CMD args and return a tuble with them
 
     Returns:
         Tuple[str, str]: (source directory, destination directory)
     """
-    parser = argparse.ArgumentParser(description="🍅🍅 task",
-                                      formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument('-version', action='version', version='Version ' + "0.0.5",
-                        help='laZagne version')
+    parser = argparse.ArgumentParser(
+        description="🍅🍅 task", formatter_class=argparse.RawTextHelpFormatter
+    )
+    parser.add_argument(
+        "-version",
+        action="version",
+        version="Version " + "0.0.5",
+        help="laZagne version",
+    )
 
-    parser.add_argument('-s',
-                        '--source',  
-                        default=os.path.join(os.path.abspath(os.path.join(os.getcwd(), "..")), "files") ,
-                        help='source dir where the unsorted files live')
-    
-    parser.add_argument('-d',
-                        '--dist',  
-                        default=os.path.join(os.path.abspath(os.path.join(os.getcwd(), "..")), "organized_files_dir") ,
-                        help='dir where the files will be sorted in')
-    
+    parser.add_argument(
+        "-s",
+        "--source",
+        default=os.path.join(os.path.abspath(os.path.join(os.getcwd(), "..")), "files"),
+        help="source dir where the unsorted files live",
+    )
+
+    parser.add_argument(
+        "-d",
+        "--dist",
+        default=os.path.join(
+            os.path.abspath(os.path.join(os.getcwd(), "..")), "organized_files_dir"
+        ),
+        help="dir where the files will be sorted in",
+    )
+
     args = parser.parse_args()
     return (args.source, args.dist)
 
-def organize_files(root_dir: str, dist_dir:str) -> int:
+
+def organize_files(root_dir: str, dist_dir: str) -> int:
     """Organizes the files from the `source dir` to the `dist dir`
 
     Args:
@@ -58,14 +71,17 @@ def organize_files(root_dir: str, dist_dir:str) -> int:
     try:
         unsorted_files = os.listdir(root_dir)
 
-        if len(unsorted_files) > 0: 
+        if len(unsorted_files) > 0:
             for file_path in os.listdir(root_dir):
                 file_name = file_path.split("-")[0]
 
-                create_dir_if_doesnt_exist(os.path.join(dist_dir,file_name))
+                create_dir_if_doesnt_exist(os.path.join(dist_dir, file_name))
 
-                if file_path.find(file_name) >= 0 :
-                    move_file(os.path.join(root_dir, file_path), os.path.join(dist_dir,file_name))
+                if file_path.find(file_name) >= 0:
+                    move_file(
+                        os.path.join(root_dir, file_path),
+                        os.path.join(dist_dir, file_name),
+                    )
             return len(os.listdir(dist_dir))
         print("there are NO files to sort Q~Q!")
         return -1
@@ -74,17 +90,14 @@ def organize_files(root_dir: str, dist_dir:str) -> int:
         print("not really needed or placed correctly but meh")
         return -1
 
+
 if __name__ == "__main__":
-
-
     root_dir, organized_files_dir = parse_args()
-    
+
     total_files_sorted = organize_files(root_dir, organized_files_dir)
 
     # simulating a usage or something xD
-    if total_files_sorted == -1 :
+    if total_files_sorted == -1:
         print("do something depening on the failure")
     else:
         print("do something depening on the sucess")
-
-
